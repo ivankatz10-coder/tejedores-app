c76a147e450a5bd66b309f25e8fb60db4f065941
ivankatz10-coder <ivankatz10@gmail.com>
2026-03-25 12:48:41 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 5000,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
